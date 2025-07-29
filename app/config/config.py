a2adb45aece789b33f732ef8be35ffc8764eb68d
randynolden <rnolden2@gmail.com>
2025-07-28 23:58:13 -0700

    fix: genai config

--- app/config/config.py
+++ app/config/config.py
@@ -1,6 +1,6 @@
 import orjson
 from openai import OpenAI
-import google.generativeai as genai
+from google import genai
 from ..models.model_list import openai_models, google_models, perplexity_models
 from ..agent_schema.agent_master_schema import AgentModel
 from google.cloud import secretmanager
